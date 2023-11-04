acc9d89ee3d2b5b368dbf4a0777f03bba073de80
khemjiratha <140263331+khemjiratha@users.noreply.github.com>
2023-11-04 14:32:13 +0700

    unit 5 6

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,23 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+require('./route')(app)
+
 app.get('/status', function (req,res){
-    res.send('Hello nodejs server eiei')
+    res.send('Hello nodejs server eiei khemjira')
 })
 
-app.get('/hello/:person', function(req,res){
-    console.log('hello - '+req.params.person)
-    res.send('say hello with '+req.params.person)
+app.get('/hello/:name', function(req,res){
+    console.log('hello - ' + req.params.name)
+    res.send('say hello with ' + req.params.name)
 })
-
 
 let port = 8081
-
 app.listen(port, function(){
-    console.log('server running on '+port)
-})
+    console.log('server runing on ' + port)
+})  
